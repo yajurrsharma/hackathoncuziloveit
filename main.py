6f5e1795370d276e1efe1bd0300beb4416ab240f
Yajur Sharma <155890182+yajurrsharma@users.noreply.github.com>
2025-08-17 13:30:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pyttsx3
 
 app = Flask(__name__)
 
-VIDEO_URL = "http://192.168.1.2:4444"   
+VIDEO_URL = "http://<IP>:<PORT>"   
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
@@ -66,3 +66,4 @@ def caption():
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
+
